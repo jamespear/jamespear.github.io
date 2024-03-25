d471ebc42dc923d6cb5ff6cc77585ee27da07bfb
James Pearce <jamespearce@berkeley.edu>
2024-03-24 23:50:24 -0400

    commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-document.getElementsByTagName("input")[0].addEventListener("keydown", checkKeyPressed);
-
-function checkKeyPressed(event) {
-    if(event.code == "Enter") {
-        alert("You pressed 'enter'");
-    }
-}
-
 if (window.location.href==="https://jamespear.github.io") {
-    window.location.href = "https://cal-cs184-student.github.io/hw-webpages-sp24-jamespear"; 
+    window.location.href = "https://cal-cs184-student.github.io/hw-webpages-sp24-jamespear";
 }
+
+// document.getElementsByTagName("input")[0].addEventListener("keydown", checkKeyPressed);
+//
+// function checkKeyPressed(event) {
+//     if(event.code == "Enter") {
+//         alert("You pressed 'enter'");
+//     }
+// }
